e70f64cff879b0f38b39a1ebd6dfddb49edbe5ea
Divyansh8x <Divyansh8x@users.noreply.github.com>
2024-05-25 14:10:29 +0530

    Final Commit!

--- app/Rating.jsx
+++ app/Rating.jsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
-import Typography from '@mui/material/Typography';
 
 export default function BasicRating() {
   const [value, setValue] = useState(2);
